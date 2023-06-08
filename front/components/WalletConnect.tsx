f92963254a38e827aa8feee26e7d26d6e32564bc
T4D4 <t4d4.icp@gmail.com>
2023-06-08 23:14:30 +0900

    deleate blank

--- front/components/WalletConnect.tsx
+++ front/components/WalletConnect.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import { 
-    ExternalProvider, 
-    JsonRpcSigner, 
-    Network, 
-    Web3Provider 
+import {
+    ExternalProvider,
+    JsonRpcSigner,
+    Network,
+    Web3Provider
   } from '@ethersproject/providers';
 
 
@@ -22,7 +22,6 @@ export default function WalletConnect (): any {
                 await window.ethereum.request({ method: 'eth_requestAccounts' });
                 const provider = new ethers.providers.Web3Provider(window.ethereum);
                 const signer = provider.getSigner();
-        
                 // 接続されたアカウントの情報を取得
                 const address = await signer.getAddress();
                 console.log('Connected address:', address);
@@ -37,7 +36,7 @@ export default function WalletConnect (): any {
 
     return(
         <div>
-            <button 
+            <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
             onClick={connectToMetamask}
             >
