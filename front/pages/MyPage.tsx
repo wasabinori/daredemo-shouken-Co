99325461b6cea79a25e838f5023b5579ce21c482
T4D4 <t4d4.icp@gmail.com>
2023-06-09 22:42:45 +0900

    MyPage８割完成。あとは適当にデザインつけるぐらいの筈

--- front/pages/MyPage.tsx
+++ front/pages/MyPage.tsx
@@ -26,7 +26,7 @@ const fetchData = async () => {
     useEffect(() => {
         const checkWalletConnection = async () => {
             // Walletが接続されているかを確認する処理
-            if (typeof window.ethereum !== 'undefined') {
+            if (typeof window.ethereum === 'undefined') {
                 try {
                     // MetamaskなどのEthereumプロバイダに接続を試みる
                     await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -54,15 +54,23 @@ const fetchData = async () => {
                 {item.totalCount === 0 && <div>You have no NFT.</div>}
                 {item.totalCount === 1 && <div>You have 1 NFT.</div>}
                 {item.totalCount >= 2 && <div>You have {item.totalCount} NFTs.</div>}
-                <div>pageKey:{item.pageKey}</div>
-                <div>balance: {item.ownedNfts[0].balance}</div>
-                <div>Contract.address: {item.ownedNfts[0].contract.address}</div>
-                <div>Contract.address: {item.ownedNfts[1].contract.address}</div>
-                <div>description: {item.ownedNfts[0].description}</div>
-                <div>timeLastUpdated: {item.ownedNfts[0].timeLastUpdated}</div>
-                <div>Title: {item.ownedNfts[0].title}</div>
-                <div>tokenId: {item.ownedNfts[0].tokenId}</div>
-                <div>tokenType: {item.ownedNfts[0].tokenType}</div>
+                <div>
+                    {item.totalCount > 0 && (
+                        <div>
+                            {item.ownedNfts.map((item, index) => (
+                                <div key={index}>
+                                    <div>NFT: {index + 1}</div>
+                                    <div>Title:「{item.title}」</div>
+                                    <div>TokenID: {item.tokenId}</div>
+                                    <div>Address: {item.contract.address}</div>
+                                    <div>Description:「{item.description}」</div>
+                                    <div>Balance: {item.balance}</div>
+                                    <br />
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
             </div>
             ))}
         </div>
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,13 +4,18 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "" // ここにテーマを追加。最後に暇があれば考える
+    ]
+  }
 }
 
