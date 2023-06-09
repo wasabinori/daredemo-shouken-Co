3d724f1eac8df81ff81b5a0b4cdee95766679a60
T4D4 <t4d4.icp@gmail.com>
2023-06-09 21:08:40 +0900

    Alchemyのお陰で所持NFTの情報を取得できるようになった。一部はなんか取ってこれないっぽい？

--- front/pages/MyPage.tsx
+++ front/pages/MyPage.tsx
@@ -1,8 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import { Network, initializeAlchemy, getNftsForOwner, OwnedNftsResponse } from "@alch/alchemy-sdk";
+
 
 const Mypage = () => {
     const [connected, setConnected] = useState(false);
     const [address, setAddress] = useState('');
+    const [NFTs, setNFTs] = useState<OwnedNftsResponse[]>([]);
+
+const fetchData = async () => {
+    const settings = {
+        apikey: "0pKIAXtqho6DikSSdbB-QuyB1dyjwfCy",
+        network: Network.ETH_GOERLI,
+        maxRetries: 10,
+    };
+    const alchemy = initializeAlchemy(settings);
+    // 接続されたアカウントの情報を取得
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    setAddress(accounts[0]);
+    const tmpNFTs = await getNftsForOwner(alchemy, accounts[0]);
+
+    setNFTs([tmpNFTs]);
+};
+
 
     useEffect(() => {
         const checkWalletConnection = async () => {
@@ -12,10 +31,7 @@ const Mypage = () => {
                     // MetamaskなどのEthereumプロバイダに接続を試みる
                     await window.ethereum.request({ method: 'eth_requestAccounts' });
                     setConnected(true);
-                    // 接続されたアカウントの情報を取得
-                    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-                    setAddress(accounts[0]);
-                    
+                    fetchData();
                 } catch (error) {
                     console.error('Failed to connect to Metamask:', error);
                 }
@@ -31,11 +47,25 @@ const Mypage = () => {
     }
 
     return (
-    <div>
-        <h1>アドレス: {address}</h1>
-        <h2>所持しているNFT一覧:</h2>
-        
-    </div>
+        <div>
+            <h1>{address}</h1>
+            {Array.isArray(NFTs) && NFTs.map((item, index) => (
+            <div key={index}>
+                {item.totalCount === 0 && <div>You have no NFT.</div>}
+                {item.totalCount === 1 && <div>You have 1 NFT.</div>}
+                {item.totalCount >= 2 && <div>You have {item.totalCount} NFTs.</div>}
+                <div>pageKey:{item.pageKey}</div>
+                <div>balance: {item.ownedNfts[0].balance}</div>
+                <div>Contract.address: {item.ownedNfts[0].contract.address}</div>
+                <div>Contract.address: {item.ownedNfts[1].contract.address}</div>
+                <div>description: {item.ownedNfts[0].description}</div>
+                <div>timeLastUpdated: {item.ownedNfts[0].timeLastUpdated}</div>
+                <div>Title: {item.ownedNfts[0].title}</div>
+                <div>tokenId: {item.ownedNfts[0].tokenId}</div>
+                <div>tokenType: {item.ownedNfts[0].tokenType}</div>
+            </div>
+            ))}
+        </div>
     );
 };
 
